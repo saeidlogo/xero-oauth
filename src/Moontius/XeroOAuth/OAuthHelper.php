a87c4badbdf5ffb9ba9c98b7822203e4c4b95b6d
Saeid <saeid.kh@basketasia.com>
2018-02-21 16:21:44 +0330

    Remove request coupling from xero API methods

--- src/Moontius/XeroOAuth/OAuthHelper.php
+++ src/Moontius/XeroOAuth/OAuthHelper.php
@@ -9,8 +9,7 @@
 namespace Moontius\XeroOAuth;
 
 use Moontius\XeroOAuth\Lib\XeroOAuth;
-use Illuminate\Http\Request;
-use Exception;
+use Moontius\XeroOAuth\XeroException;
 
 /**
  * Description of OAuthHelper
@@ -61,60 +60,55 @@ class OAuthHelper {
         }
     }
 
-    public function authenticate(Request $request): string {
-        $session = $request->session();
+    public function authenticate(): string {
         $params = array(
             'oauth_callback' => $this->callback
         );
         $this->xeroOAuth->request('GET', $this->xeroOAuth->url('RequestToken', ''), $params);
         if ($this->xeroOAuth->response ['code'] == 200) {
-            return $this->authorize($session);
+            return $this->authorize();
         } else {
             return $this->xeroOAuth->response['response'];
         }
     }
 
-    public function authorize($session): string {
-        //            print_r($this->xeroOAuth->extract_params($this->xeroOAuth->response ['response']));
+    public function authorize(): string {
         $scope = '';
         $response = $this->xeroOAuth->extract_params($this->xeroOAuth->response ['response']);
-        $session->put('oauth', $response);
+        $this->xero_session_put('oauth', $response);
         $authurl = $this->xeroOAuth->url("Authorize", '') . "?oauth_token={$response['oauth_token']}&scope=" . $scope;
         return $authurl;
     }
 
-    function callback(Request $request): string {
-        $session = $request->session();
-        $oauth = $session->get('oauth');
+    function callback($params): string {
+        $oauth = $this->xero_session_get('oauth');
         $this->xeroOAuth->config ['access_token'] = $oauth['oauth_token'];
         $this->xeroOAuth->config ['access_token_secret'] = $oauth['oauth_token_secret'];
 
         $this->xeroOAuth->request('GET', $this->xeroOAuth->url('AccessToken', ''), array(
-            'oauth_verifier' => $request->input('oauth_verifier'),
-            'oauth_token' => $request->input('oauth_token')
+            'oauth_verifier' => $params['oauth_verifier'],
+            'oauth_token' => $params['oauth_token']
         ));
 
         if ($this->xeroOAuth->response ['code'] == 200) {
             $response = $this->xeroOAuth->extract_params($this->xeroOAuth->response ['response']);
             if (isset($response)) {
-                $session->put('oauth_token', $response['oauth_token']);
-                $session->put('oauth_token_secret', $response['oauth_token_secret']);
+                $this->xero_session_put('oauth_token', $response['oauth_token']);
+                $this->xero_session_put('oauth_token_secret', $response['oauth_token_secret']);
                 if (isset($response['oauth_session_handle']))
-                    $session->put('session_handle', $response['oauth_session_handle']);
+                    $this->xero_session_put('session_handle', $response['oauth_session_handle']);
             } else {
                 return false;
             }
-            $session->remove('oauth');
-//            unset($_SESSION ['oauth']);
+            $this->xero_session_destroy('oauth');
             return 'success';
         }
 
         return $this->xeroOAuth->response['response'];
     }
 
-    function refresh(Request $request) {
-        $session = $request->session();
-        $response = $this->xeroOAuth->refreshToken($session->get('oauth_token'), $session->get('oauth_session_handle'));
+    function refresh() {
+        $response = $this->xeroOAuth->refreshToken($this->xero_session_destroy('oauth_token'), $this->xero_session_destroy('oauth_session_handle'));
         if ($this->xeroOAuth->response['code'] == 200) {
             $session = persistSession($response);
             $oauthSession = retrieveSession();
@@ -124,48 +118,94 @@ class OAuthHelper {
         }
     }
 
-    function logout(Request $request) {
-        $session = $request->session();
-        $session->remove('oauth_token');
-        $session->remove('xero');
+    function logout() {
+        $this->xero_session_destroy('oauth_token');
+        $this->xero_session_destroy('xero');
     }
 
-    public function all_organisations(Request $request) {
-        if ($this->xeroOAuth($request)) {
+    public function all_organisations() {
+        if ($this->xero_auth()) {
             $xml = '';
-            $this->xeroOAuth->request('GET', $this->xeroOAuth->url('Organisation', 'core'), array(), $xml, 'json');
+            $this->xeroOAuth->request('GET', $this->xeroOAuth->url('Organisations', 'core'), array(), $xml, 'json');
             if ($this->xeroOAuth->response['code'] == 200) {
                 $organisation = $this->xeroOAuth->parseResponse($this->xeroOAuth->response['response'], $this->xeroOAuth->response['format']);
                 $json = json_decode(json_encode($organisation), true);
                 return $json;
+            }
+            $json = $this->xeroOAuth->response['response'];
+            if (!is_null($json)) {
+                //if $json is not array maybe there is a error
+                if (!is_array($json)) {
+                    //convert query string to json format
+                    $error = $oauth->query_string_to_json($json);
+                    if (isset($error['oauth_problem'])) {
+                        switch ($error['oauth_problem']) {
+                            case 'token_expired'://if xero token is expired we need to get fresh token and client should be redirect to auth page
+                                throw new XeroException('expired token xero account', 501);
+                            default;
+                                throw new XeroException($error['oauth_problem'], 501);
+                        }
+                    }
+                    throw new XeroException($error, 501);
+                }
+                if (isset($json['Organisations'])) {
+                    return $json['Organisations'];
+                }
+                throw new XeroException('invalid json file', 501);
+            }
+            throw new XeroException('invalid response from xero account organisation list', 501);
+        }
+        throw new XeroException('xero authentication error', 501);
+    }
+
+    public function xero_auth(): bool {
+        $token = $this->xero_session_get('oauth_token');
+        if (null !== $token) {
+            $this->xeroOAuth->config['access_token'] = $token;
+            $this->xeroOAuth->config['access_token_secret'] = $this->xero_session_get('oauth_token_secret');
+            $this->xeroOAuth->config['session_handle'] = $this->xero_session_get('oauth_session_handle');
+            return true;
+        }
+        return false;
+    }
+
+    public function bank_accounts() {
+        if ($this->xero_auth()) {
+            $this->xeroOAuth->request('GET', $this->xeroOAuth->url('Accounts', 'core'), array('Where' => 'Type=="BANK"'), '', 'json');
+            if ($this->xeroOAuth->response['code'] == 200) {
+                $accounts = $this->xeroOAuth->parseResponse($this->xeroOAuth->response['response'], $this->xeroOAuth->response['format']);
+                return $accounts->Accounts;
             }
             return $this->xeroOAuth->response['response'];
         }
         return null;
     }
 
-    public function xeroOAuth(Request $request): bool {
-        $session = $request->session();
-        if (null !== $session->get('oauth_token')) {
-            $this->xeroOAuth->config['access_token'] = $session->get('oauth_token');
-            $this->xeroOAuth->config['access_token_secret'] = $session->get('oauth_token_secret');
-            $this->xeroOAuth->config['session_handle'] = $session->get('oauth_session_handle');
-            return true;
+    public function xero_session_get($key) {
+        if (isset($_SESSION['XERO'][$key])) {
+            return $_SESSION['XERO'][$key];
         }
-        return false;
+        return null;
     }
 
-    public function bank_accounts(Request $request) {
-        $this->xeroOAuth->request('GET', $this->xeroOAuth->url('Accounts', 'core'), array('Where' => 'Type=="BANK"'), '', 'json');
-        if ($this->xeroOAuth->response['code'] == 200) {
-            $accounts = $this->xeroOAuth->parseResponse($this->xeroOAuth->response['response'], $this->xeroOAuth->response['format']);
-            return $accounts->Accounts;
-        }
-        return $this->xeroOAuth->response['response'];
+    public function xero_session_put($key, $value) {
+        $_SESSION['XERO'][$key] = $value;
     }
 
-}
+    public function xero_session_destroy($key) {
+        if (isset($_SESSION['XERO'][$key])) {
+            unset($_SESSION['XERO'][$key]);
+        }
+    }
 
-class XeroException extends \Exception {
-    
+    public function query_string_to_json($string) {
+        $keys = preg_split("/[\s,=,&]+/", $string);
+        $arr = array();
+        for ($i = 0; $i < sizeof($keys); $i++) {
+            $arr[$keys[$i]] = $keys[++$i];
+        }
+        $result = (object) $arr;
+        return json_decode(json_encode($result), true);
+    }
+
 }
